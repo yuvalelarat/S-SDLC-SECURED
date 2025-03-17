d24f57f6b801097e421c9df70b977a79e70a602e
yuvalelarat <yuvalelarat@gmail.com>
2025-03-17 23:16:13 +0200

    minor fix

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -2,25 +2,20 @@ import { useNavigate } from "react-router-dom";
 
 export function NavBar() {
   const navigate = useNavigate();
+
+  const pStyles =
+    "px-4 py-1 text-black font-bold hover:cursor-pointer hover:underline hover:scale-110 transition-all duration-300 ease-in-out";
+
   return (
     <div className="flex justify-center items-center bg-gray-100 p-4 fixed top-0 w-full z-10">
       <div className="flex space-x-4 border-b-2 border-b-blue-400 w-80 items-center justify-evenly text-lg">
-        <p
-          className="px-4 py-1 text-black font-bold hover:cursor-pointer hover:underline hover:scale-110 transition-all duration-300 ease-in-out"
-          onClick={() => navigate("/")}
-        >
+        <p className={pStyles} onClick={() => navigate("/")}>
           Home
         </p>
-        <p
-          className="px-4 py-1 text-black font-bold hover:cursor-pointer hover:underline hover:scale-110 transition-all duration-300 ease-in-out"
-          onClick={() => navigate("/login")}
-        >
+        <p className={pStyles} onClick={() => navigate("/login")}>
           Login
         </p>
-        <p
-          className="px-4 py-1 text-black font-bold hover:cursor-pointer hover:underline hover:scale-110 transition-all duration-300 ease-in-out"
-          onClick={() => navigate("/register")}
-        >
+        <p className={pStyles} onClick={() => navigate("/register")}>
           Register
         </p>
       </div>
