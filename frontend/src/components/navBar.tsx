91e9918b3a4d4c1beeff753339599eededd14d7b
‪Yuval Elarat‬‏ <yuvalelarat@gmail.com>
2025-04-24 23:29:26 +0300

    [feat]: fetch clients to auth user

--- frontend/src/components/navBar.tsx
+++ frontend/src/components/navBar.tsx
@@ -35,7 +35,7 @@ export function NavBar() {
     "px-4 py-1 text-black font-bold hover:cursor-pointer hover:underline hover:scale-110 transition-all duration-300 ease-in-out";
 
   return (
-    <div className="flex justify-center items-center p-4 fixed top-0 w-full z-10">
+    <div className="bg-[#f3f4f6] flex justify-center items-center p-4 fixed top-0 w-full z-10">
       <div className="flex space-x-4 border-b-2 border-b-blue-400 w-96 items-center justify-evenly text-lg">
         <p className={pStyles} onClick={() => navigate("/")}>
           Home
--- frontend/src/components/whiteCard.tsx
+++ frontend/src/components/whiteCard.tsx
@@ -1,11 +1,12 @@
 interface WhiteCardProps {
   children: React.ReactNode;
+  middle?: boolean;
 }
 
-export function WhiteCard({ children }: WhiteCardProps) {
+export function WhiteCard({ children, middle= true }: WhiteCardProps) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full">
-      <div className="bg-white p-6 rounded shadow-md w-96">{children}</div>
+    <div className={`flex flex-col items-center justify-center ${middle ? "min-h-screen" : ''} bg-gray-100 w-full`}>
+      <div className={`bg-white p-6 rounded shadow-md ${middle ? 'w-96' : ''}`}>{children}</div>
     </div>
   );
 }
